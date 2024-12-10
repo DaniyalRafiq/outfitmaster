d9869469860dd4037b41a1fda7c620b97ac2709b
daniyal rafiq <bb420f@gmail.com>
2024-12-11 01:20:08 +0500

    done

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+
+
+// Get Quote Form validation
+
+(function () {
+  'use strict';
+  var forms = document.querySelectorAll('.needs-validation');
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
+
+
+$(document).ready(function () {
+  $('.navdesktop').hover(
+    function () {
+      $(this).addClass('show');
+      $(this).find('.dropdown-navdesktop').first().addClass('show');
+    },
+    function () {
+      $(this).removeClass('show');
+      $(this).find('.dropdown-navdesktop').first().removeClass('show');
+    }
+  );
+});
+
+// Sticky header
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+// end Sticky header
+
+// client Slick Slider
+
+
